1084c2f7642e4cd7ea36b27ddf57497d95f8b351
Fırat Yıldız <firat.yildiz@quants.com>
2024-10-28 11:31:30 +0300

    add not found error

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,11 @@
 import express, { json } from "express"
+import "express-async-errors"
 import { currentUserRouter } from "./routes/currentUser"
 import { signInRouter } from "./routes/signin"
 import { signOutRouter } from "./routes/signout"
 import { signUpRouter } from "./routes/signup"
 import { errorHandler } from "./middlewares/errorHandler"
+import { NotFoundError } from "./errors/NotFoundError"
 
 const app = express()
 app.use(json())
@@ -13,6 +15,10 @@ app.use(currentUserRouter)
 app.use(signUpRouter)
 app.use(signInRouter)
 app.use(signOutRouter)
+
+app.all("*", () => {
+    throw new NotFoundError()
+})
 
 app.use(errorHandler)
 
