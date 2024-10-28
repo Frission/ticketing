00557dbb6d44176b465e1700918c204927a50d7a
Fırat Yıldız <fyildiz1597@gmail.com>
2024-10-28 14:59:37 +0300

    add user mongoose schema

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,14 @@
 import express, { json } from "express"
 import "express-async-errors"
+import mongoose from "mongoose"
+
 import { currentUserRouter } from "./routes/currentUser"
 import { signInRouter } from "./routes/signin"
 import { signOutRouter } from "./routes/signout"
 import { signUpRouter } from "./routes/signup"
 import { errorHandler } from "./middlewares/errorHandler"
 import { NotFoundError } from "./errors/NotFoundError"
+import { DatabaseConnectionError } from "./errors/DatabaseConnectionError"
 
 const app = express()
 app.use(json())
@@ -22,6 +25,17 @@ app.all("*", () => {
 
 app.use(errorHandler)
 
-app.listen(3000, () => {
+const start = async () => {
+    try {
+        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth")
+        console.log("Connected to Mongo DB")
+    } catch (err) {
+        console.error(err)
+        throw new DatabaseConnectionError()
+    }
+}
+
+app.listen(3000, async () => {
+    await start()
     console.log("Listening on port 3000")
 })
