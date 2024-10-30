3f4738b0518e9fe561ec91278efedd351e075385
Fırat Yıldız <fyildiz1597@gmail.com>
2024-10-30 14:57:34 +0300

    finish signup and signin logic

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,38 @@
-import express from "express"
+import express, { Request, Response } from "express"
+import { body } from "express-validator"
+import { validateRequest } from "../middlewares/validateRequest"
+import { User } from "../models/User"
+import { BadRequestError } from "../errors/BadRequestError"
+import { PasswordUtils } from "../util/PasswordUtils"
+import jwt from "jsonwebtoken"
 
 const router = express.Router()
 
-router.get("/api/users/signin", (req, res) => {
-    
+const middlewares = [
+    body("email").isString().isEmail().isLength({ min: 3, max: 250 }).withMessage("E-mail must be valid"),
+    body("password").isString().trim().notEmpty().withMessage("You must provide a password"),
+    validateRequest,
+]
+
+router.get("/api/users/signin", middlewares, async (req: Request, res: Response) => {
+    const { email, password } = req.body
+
+    const existingUser = await User.findOne({ email })
+    if (existingUser == null) {
+        throw new BadRequestError("E-mail or password was incorrect")
+    }
+
+    const passwordCorrect = await PasswordUtils.compare(existingUser.password, password)
+    if (!passwordCorrect) {
+        throw new BadRequestError("E-mail or password was incorrect")
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign({ id: existingUser.id, email: existingUser.email }, process.env.JWT_KEY!)
+
+    req.session = { jwt: userJwt }
+
+    res.send(existingUser)
 })
 
 export { router as signInRouter }
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,24 +3,22 @@ import { body, validationResult } from "express-validator"
 import { BadRequestError } from "../errors/BadRequestError"
 import { RequestValidationError } from "../errors/RequestValidationError"
 import { User } from "../models/User"
+import jwt from "jsonwebtoken"
+import { validateRequest } from "../middlewares/validateRequest"
 
 const router = express.Router()
 
-const validators = [
+const middlewares = [
     body("email").isString().isEmail().isLength({ min: 3, max: 250 }).withMessage("E-mail must be valid"),
     body("password")
         .isString()
         .trim()
         .isLength({ min: 4, max: 20 })
         .withMessage("Password must be between 4 and 20 characters"),
+        validateRequest
 ]
 
-router.post("/api/users/signup", validators, async (req: Request, res: Response) => {
-    const errors = validationResult(req)
-
-    if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array())
-    }
+router.post("/api/users/signup", middlewares, async (req: Request, res: Response) => {
     console.log("Creating a user...")
 
     const { email, password } = req.body
@@ -31,6 +29,11 @@ router.post("/api/users/signup", validators, async (req: Request, res: Response)
 
     const user = User.build({ email, password })
     await user.save()
+
+    // Generate JWT
+    const userJwt = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_KEY!)
+
+    req.session = { jwt: userJwt }
 
     res.status(201).send(user)
 })
