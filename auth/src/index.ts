26eae439265d3f078bc73ae6a2fb85aa4dc6962e
Fırat Yıldız <firat.yildiz@quants.com>
2024-11-08 11:47:51 +0300

    move mongo uri to k8s config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,8 +7,12 @@ const start = async () => {
         throw new Error("JWT key must be defined")
     }
 
+    if (!process.env.MONGO_URI) {
+        throw new Error("ENV: Mongo URI must be defined.")
+    }
+
     try {
-        await mongoose.connect("mongodb://auth-mongo-srv:27017/auth")
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("Connected to Mongo DB")
     } catch (err) {
         console.error(err)
