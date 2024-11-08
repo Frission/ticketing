506a12244d11852874d09c946b5a4ac55bb5eb5f
Fırat Yıldız <fyildiz1597@gmail.com>
2024-11-08 14:06:14 +0300

    add tests, add Ticket model

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,8 @@ import express, { json } from "express"
 import "express-async-errors"
 
 import cookieSession from "cookie-session"
-import { NotFoundError, errorHandler } from "@frissionapps/common"
+import { NotFoundError, currentUser, errorHandler } from "@frissionapps/common"
+import { createTicketRouter } from "./routes/new"
 
 const app = express()
 app.set("trust proxy", true)
@@ -13,6 +14,9 @@ app.use(
         secure: process.env.NODE_ENV !== "test",
     }),
 )
+app.use(currentUser)
+
+app.use(createTicketRouter)
 
 app.all("*", () => {
     throw new NotFoundError()
--- tickets/src/test/signUpHelper.ts
+++ tickets/src/test/signUpHelper.ts
@@ -1,6 +1,11 @@
-
 import { app } from "../app"
+import jwt from "jsonwebtoken"
 
 export const testSignUp = async (credentials: { email?: string; password?: string } = {}) => {
+    const payload = { email: "test@test.com", password: "test123", ...credentials }
 
+    const session = { jwt: jwt.sign(payload, process.env.JWT_KEY!) }
+    const sessionBase64 = Buffer.from(JSON.stringify(session)).toString("base64")
+
+    return [`session=${sessionBase64}`]
 }
